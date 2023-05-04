80a292222256e1680a4dec6e8e7f9675e87f171b
rekil2g <kevinlinux2020@gmail.com>
2023-05-04 07:32:45 -0300

    ola mundo em java adicionado

---
+++ olamundo/src/Olamundo.java
@@ -0,0 +1,11 @@
+
+public class Olamundo {
+
+	public static void main(String[] args) {
+		// Primeiro codigo java para o GitHub.
+		System.out.println("Ola mundo!");
+		System.out.println("Primeiro codigo do curso de java enviado para o GitHub!");
+
+	}
+
+}
